72d72669f6e9d10dd4dfec9d40b335a618c47897
Dylan Guillon <dylan.guillon.04@gmail.com>
2026-01-12 16:28:05 +0100

    updated crew button on header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,7 +33,8 @@
 		<ul class="flex items-center gap-5">
 			{#if data.user}
 				<!-- https://www.svgrepo.com/svg/532088/bell -->
-				<a class="bg-amber-400 font-bold py-2 px-4 rounded text-white" href="/crew">Mon équipage</a>
+				<a class="bg-gradient-to-br from-amber-400 via-orange-400 to-orange-500 font-semibold px-4 py-2 rounded-md shadow text-lg text-black
+				hover:bg-gradient-to-br hover:from-orange-400 hover:via-orange-500 hover:to-orange-500 hover:underline" href="/crew">Mon équipage</a>
 				<li class="nav-link"><a href="/notifications"><img class="h-5" src="{notification_icon}" alt=""></a></li>
 				<li class="nav-link font-bold"><a href="/manage_account">Profil</a></li>
 				<li class="nav-link"><form method="POST" action="/logout">
